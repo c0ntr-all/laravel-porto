e6a13b8aeb78394da8a33dff261222fbeb1c417f
c0ntr-all <serg.taturin@gmail.com>
2024-08-12 23:17:41 +0300

    build(client/deps): configure axios
    Installed axios types and configured axios interceptors to handle requests auth

--- client/src/boot/axios.ts
+++ client/src/boot/axios.ts
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import axios, { AxiosInstance } from 'axios'
+import { Notify } from 'quasar'
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
@@ -14,10 +15,49 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({ baseURL: `${process.env.host}/` })
 
-export default boot(({ app }) => {
+export default boot(({ app, router }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
+  api.interceptors.request.use(config => {
+    config.headers.accept = 'application/json'
+    if (localStorage.access_token) {
+      config.headers.authorization = `Bearer ${localStorage.access_token}`
+    }
+
+    return config
+  }, error => {
+    return Promise.reject(error)
+  })
+
+  api.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response) {
+        // Auth errors
+        if (error.response.status === 401) {
+          localStorage.removeItem('access_token')
+          router.push('/login')
+        }
+      } else if (error.request) {
+        // Errors that did not receive a response from the server (including CORS errors)
+        if (error.message.includes('Network Error')) {
+          Notify.create({
+            type: 'negative',
+            message: 'A network error has occurred. Please check your connection or CORS settings.'
+          })
+        }
+      } else {
+        // Other errors
+        Notify.create({
+          type: 'negative',
+          message: error.message
+        })
+      }
+
+      return Promise.reject(error)
+    }
+  )
 
   app.config.globalProperties.$axios = axios
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)

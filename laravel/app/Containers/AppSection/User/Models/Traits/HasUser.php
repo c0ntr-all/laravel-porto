c4ad003e319f384cccfba120190ad87eee09111f
c0ntr-all <serg.taturin@gmail.com>
2024-08-14 23:49:37 +0300

    feat(laravel/users): add trait with global scope
    Added trait HasUsers for other containers. GlobalScope included.

---
+++ laravel/app/Containers/AppSection/User/Models/Traits/HasUser.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Containers\AppSection\User\Models\Traits;
+
+use App\Containers\AppSection\User\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait HasUser
+{
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::addGlobalScope('user', function ($builder) {
+            $builder->where(static::make()->getTable() . '.user_id', '=', auth()->user()->id);
+        });
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}

560791443464532b219a0ebf496d466569f4616d
c0ntr-all <serg.taturin@gmail.com>
2024-08-20 23:48:00 +0300

    feat(client/task manager): add task list component
    Added base task list component with using typescript

---
+++ client/src/components/client/TaskManager/TaskList/AppTaskList.vue
@@ -0,0 +1,210 @@
+<template>
+  <q-card class="list bg-grey-4">
+    <q-card-section class="list__header">
+      <p>{{ list.title }}</p>
+    </q-card-section>
+    <q-separator dark/>
+    <q-card-section class="list__body">
+      <TaskItem v-for="task in tasks" :key="task.id" :item="task"/>
+    </q-card-section>
+    <q-card-section class="list__footer">
+      <div v-if="!showAddForm" @click="openAddForm" class="list__add-button">
+        <q-icon name="add" size="sm"/>
+        <span>Добавить карточку</span>
+      </div>
+      <div v-if="showAddForm" class="list__add-form">
+        <q-input
+          @keyup.enter="addNewTask"
+          v-model="model.newCardName"
+          ref="taskAddTextarea"
+          type="textarea"
+          input-style="height: 60px; resize: none"
+          class="list__add-textarea q-mb-sm"
+          dense
+          outlined
+        />
+        <q-btn
+          @click="addNewTask"
+          label="Добавить карточку"
+          color="secondary"
+          class="q-mr-sm"
+          no-caps
+          dense
+        />
+        <q-btn
+          @click="closeAddForm"
+          icon="close"
+          color="danger"
+          size="md"
+          flat
+          round
+          dense
+        />
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts" setup>
+import { ref, nextTick } from 'vue'
+import { useQuasar } from 'quasar'
+import { AxiosError } from 'axios'
+import { api } from 'src/boot/axios'
+
+import TaskItem from 'src/components/client/TaskManager/AppTask.vue'
+
+interface Task {
+  id: string
+  title: string
+  completed: boolean
+}
+interface TaskList {
+  id: string
+  title: string
+  tasks?: Task[]
+}
+
+interface TaskAttributes {
+  title: string
+  completed: boolean
+  created_at?: string
+}
+
+interface ApiResponse {
+  data: {
+    type: string
+    id: string
+    attributes: TaskAttributes
+  },
+  meta: {
+    message?: string
+  }
+}
+
+const props = defineProps<{ list: TaskList }>()
+
+const $q = useQuasar()
+const showAddForm = ref<boolean>(false)
+const taskAddTextarea = ref<HTMLElement | null>(null)
+const tasks = ref<Task[]>(props.list.tasks || [])
+const model = ref<{ newCardName: string }>({
+  newCardName: ''
+})
+
+const openAddForm = () => {
+  showAddForm.value = true
+  nextTick(() => {
+    taskAddTextarea.value?.focus()
+  })
+}
+
+const closeAddForm = () => {
+  showAddForm.value = false
+}
+
+const addNewTask = async () => {
+  const cardName = model.value.newCardName
+  model.value.newCardName = ''
+
+  await api
+    .post<ApiResponse>('tasks', {
+      title: cardName,
+      task_list_id: props.list.id
+    })
+    .then(response => {
+      $q.notify({
+        type: 'positive',
+        message: response?.data.meta.message
+      })
+      const newTask: Task = {
+        id: response.data.data.id,
+        title: response.data.data.attributes.title,
+        completed: false
+      }
+
+      tasks.value.push(newTask)
+    })
+    .catch((error: AxiosError<{ message: string }>) => {
+      $q.notify({
+        type: 'negative',
+        message: error.response?.data.message || 'Произошла ошибка'
+      })
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+.list {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 272px;
+  max-height: 100%;
+  border-radius: 3px;
+  white-space: normal;
+  box-sizing: border-box;
+
+  &__header {
+    padding: 8px;
+
+    p {
+      margin: 0;
+    }
+
+    &-name {
+      background: #0000;
+      border-radius: 3px;
+      box-shadow: none;
+      font-weight: 600;
+      height: 28px;
+      margin: -4px 0;
+      max-height: 256px;
+      min-height: 20px;
+      padding: 4px 8px;
+      border: none;
+      resize: none;
+      overflow: hidden;
+      overflow-wrap: break-word;
+
+      &.is-active {
+        background-color: #fff;
+        box-shadow: inset 0 0 0 2px #0079bf;
+      }
+    }
+
+    &--cover {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      top: 0;
+      cursor: pointer;
+    }
+  }
+
+  &__body {
+    flex: 1 1 auto;
+    margin: 0 4px;
+    min-height: 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+    padding: 0 4px;
+  }
+
+  &__footer {
+    padding: 10px 8px;
+  }
+
+  &__add-button {
+    display: flex;
+    align-items: center;
+    border-radius: 3px;
+    padding: 5px 0;
+
+    &:hover {
+      cursor: pointer;
+      background-color: #091e4214;
+    }
+  }
+}
+</style>

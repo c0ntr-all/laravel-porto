3fff6609b6b513c3dc441d525321d6f029c321bb
c0ntr-all <serg.taturin@gmail.com>
2024-09-01 14:21:00 +0300

    feat(client/music): add base routes

--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -26,6 +26,66 @@ const routes: RouteRecordRaw[] = [
       },
       name: 'task-manager',
       alias: '/task-manager'
+    }, {
+      path: '/music',
+      component: () => import('layouts/MusicLayout.vue'),
+      meta: {
+        title: 'Music',
+        icon: 'music_note',
+        menu: true
+      },
+      children: [{
+        path: '/music',
+        component: () => import('pages/client/Music/MusicPage.vue'),
+        name: 'music',
+        props: true
+      }, {
+        path: '/music/tags/:slug',
+        component: () => import('pages/client/Music/TagPage.vue'),
+        name: 'tag',
+        props: true
+      }, {
+        path: '/music/artists/:id',
+        component: () => import('pages/client/Music/ArtistPage.vue'),
+        name: 'artist',
+        redirect: (to) => {
+          const { id } = to.params
+          return `/music/artists/${id}/albums`
+        },
+        props: true,
+        meta: {
+          title: 'Музыка'
+        },
+        children: [{
+          path: '/music/artists/:id/tracks',
+          component: () => import('pages/client/Music/ArtistPage.vue'),
+          name: 'artist-tracks',
+          props: true
+        }, {
+          path: '/music/artists/:id/albums',
+          component: () => import('pages/client/Music/ArtistPage.vue'),
+          name: 'artist-albums',
+          props: true
+        }, {
+          path: '/music/artists/:id/similar',
+          component: () => import('pages/client/Music/ArtistPage.vue'),
+          name: 'artist-similar',
+          props: true
+        }]
+      }, {
+        path: '/music/albums/:id',
+        component: () => import('pages/client/Music/AlbumPage.vue'),
+        name: 'album',
+        props: true
+      }, {
+        path: '/music/playlists/:id',
+        component: () => import('pages/client/Music/PlaylistPage.vue'),
+        name: 'playlist',
+        props: true,
+        meta: {
+          title: 'Playlists'
+        }
+      }]
     }]
   }, {
     path: '/login',

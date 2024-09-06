92001f89cc6987cae1e606569aebfec016531d68
c0ntr-all <serg.taturin@gmail.com>
2024-09-06 12:55:00 +0300

    feat(client/music): add artists list component with skeleton

---
+++ client/src/components/client/Music/MusicArtistsList.vue
@@ -0,0 +1,81 @@
+<template>
+  <template v-if="props.artists?.length">
+    <div v-if="cardMode === 'card'" class="artists-list row items-start q-gutter-md q-mb-lg">
+      <template v-if="loading">
+        <div class="column q-gutter-md q-mb-lg">
+          <MusicArtistCardRowSkeleton v-for="n in 20" :key="n"/>
+        </div>
+      </template>
+      <template v-else>
+        <MusicArtistCard
+          v-for="artist in props.artists"
+          :key="artist.id"
+          :artist="artist"
+        />
+      </template>
+    </div>
+    <div v-if="cardMode === 'row'" class="column q-gutter-md q-mb-lg">
+      <template v-if="loading">
+        <div class="column q-gutter-md q-mb-lg">
+          <MusicArtistCardRowSkeleton v-for="n in 20" :key="n"/>
+        </div>
+      </template>
+      <template v-else>
+        <MusicArtistCardRow
+          v-for="artist in props.artists"
+          :key="artist.id"
+          :artist="artist"
+        />
+      </template>
+    </div>
+    <div class="show-more-button flex justify-center">
+      <!--                <q-btn-->
+      <!--                  v-if="pagination.hasPages"-->
+      <!--                  color="primary"-->
+      <!--                  label="Show more"-->
+      <!--                  @click="loadMoreArtists"-->
+      <!--                  :loading="paginationLoading"-->
+      <!--                />-->
+    </div>
+  </template>
+  <AppNoResultsPlug v-else/>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import MusicArtistCard from 'src/components/client/Music/MusicArtistCard.vue'
+import MusicArtistCardRow from 'src/components/client/Music/MusicArtistCardRow.vue'
+import MusicArtistCardRowSkeleton from 'src/components/client/Music/MusicArtistCardRowSkeleton.vue'
+import AppNoResultsPlug from 'src/components/default/AppNoResultsPlug.vue'
+
+interface Tag {
+  id: string
+  name: string
+}
+
+interface Artist {
+  id: string
+  name: string
+  image: string
+  content: string
+  relationships: {
+    tags: {
+      data: Tag[]
+    }
+  }
+}
+
+interface Pagination {
+  perPage: number
+  hasPages: boolean
+  nextPageUrl: string
+  prevPageUrl: string
+}
+
+const props = defineProps<{
+  artists: Artist[],
+  pagination: Pagination,
+  cardMode: string
+}>()
+const loading = ref<boolean>(false)
+const cardMode = ref(props.cardMode)
+</script>
---
+++ client/src/components/client/Music/MusicArtistsListSkeleton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="column q-gutter-md q-mb-lg">
+    <AppArtistCardRowSkeleton v-for="n in 20" :key="n"/>
+  </div>
+</template>
+<script lang="ts" setup>
+import AppArtistCardRowSkeleton from 'src/components/client/Music/MusicArtistCardRowSkeleton.vue'
+</script>

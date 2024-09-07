e80812af82017f17636b0c15a0b3a079fbf6102b
c0ntr-all <serg.taturin@gmail.com>
2024-09-07 10:15:00 +0300

    feat(client/music): add back button component

---
+++ client/src/components/default/AppBackButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-btn
+    icon="arrow_back"
+    color="primary"
+    :to="link"
+  >
+    <div class="q-ml-xs">{{ text }}</div>
+  </q-btn>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  text: string,
+  link: string
+}>()
+const text = props.text
+const link = props.link
+</script>
+
+<style scoped lang="scss">
+
+</style>

5cfe5f29c4e2b4b6584f592e8c5ed2488c2f969a
c0ntr-all <serg.taturin@gmail.com>
2024-09-19 15:27:00 +0300

    feat(client/music): add playlist card with skeleton

---
+++ client/src/components/client/Music/MusicPlaylistCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-card class="playlist-card col-4">
+    <q-img :src="playlist.image" :alt="playlist.name">
+      <div class="absolute-bottom text-h6">
+        <router-link :to="`/music/playlists/${playlist.id}`" class="playlist-card__link">
+          {{ playlist.name }}
+        </router-link>
+      </div>
+    </q-img>
+  </q-card>
+</template>
+
+<script lang="ts" setup>
+interface Playlist {
+  id: string
+  name: string
+  image: string
+  created_at: string
+}
+
+const props = defineProps<{
+  playlist: Playlist
+}>()
+const playlist = props.playlist
+</script>
+
+<style lang="scss" scoped>
+.playlist-card {
+  width: 100%;
+  max-width: 250px;
+
+  &__link {
+    text-decoration: none;
+    color: #fff;
+
+    &:hover {
+      color: #ccc;
+    }
+  }
+}
+</style>
---
+++ client/src/components/client/Music/MusicPlaylistCardSkeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-card class="playlist-card col-4">
+    <q-skeleton height="200px" square />
+  </q-card>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+.playlist-card {
+  width: 100%;
+  max-width: 250px;
+
+  &__link {
+    text-decoration: none;
+    color: #fff;
+
+    &:hover {
+      color: #ccc;
+    }
+  }
+}
+</style>

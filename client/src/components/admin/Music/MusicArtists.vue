139819c9ba3308f263d36d4e52865b73329b4128
c0ntr-all <serg.taturin@gmail.com>
2024-09-25 13:24:00 +0300

    feat(client/music): add artists page for admin

---
+++ client/src/components/admin/Music/MusicArtists.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="text-h5 q-mb-md">Artists</div>
+  <q-splitter v-model="artistsSplitter">
+    <template v-slot:before>
+      <q-tabs
+        v-model="tabs"
+        vertical
+        class="text-teal"
+      >
+        <q-tab name="edit" icon="edit_square" label="Edit"/>
+        <q-tab name="upload" icon="upload" label="Upload"/>
+      </q-tabs>
+    </template>
+
+    <template v-slot:after>
+      <q-tab-panels
+        v-model="tabs"
+        animated
+        transition-prev="slide-down"
+        transition-next="slide-up"
+      >
+        <q-tab-panel name="edit">
+          <MusicArtistsEdit/>
+        </q-tab-panel>
+
+        <q-tab-panel name="upload">
+<!--          <artists-upload/>-->
+        </q-tab-panel>
+      </q-tab-panels>
+    </template>
+  </q-splitter>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import MusicArtistsEdit from 'src/components/admin/Music/MusicArtistsEdit.vue'
+// import ArtistsUpload from 'src/components/admin/music/tabs/artists/ArtistsUpload.vue'
+
+const artistsSplitter = ref(6)
+const tabs = ref('edit')
+</script>
+
+<style scoped>
+
+</style>

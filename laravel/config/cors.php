5602d5658c826a61b640728ca13ee935326cf5c0
c0ntr-all <serg.taturin@gmail.com>
2025-11-11 19:13:42 +0300

    fix(ship): correct headers and cors handle

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -17,17 +17,17 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => explode(',', env('APP_ALLOWED_ORIGINS')),
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 60 * 60 * 24, // 24 часа
 
     'supports_credentials' => false,
 

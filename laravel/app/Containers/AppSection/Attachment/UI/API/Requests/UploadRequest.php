9470255ec0442af390d0d402ab12eb012cb0419c
c0ntr-all <serg.taturin@gmail.com>
2026-06-14 22:59:34 +0300

    refactor(backend/attachments): refactor requests

--- laravel/app/Containers/AppSection/Attachment/UI/API/Requests/UploadRequest.php
+++ laravel/app/Containers/AppSection/Attachment/UI/API/Requests/UploadRequest.php
@@ -3,25 +3,15 @@
 namespace App\Containers\AppSection\Attachment\UI\API\Requests;
 
 use App\Ship\Enums\ContainerAliasEnum;
-use Illuminate\Foundation\Http\FormRequest;
+use App\Ship\Parents\Requests\AuthenticatedRequest;
 use Illuminate\Validation\Rule;
 
-class UploadRequest extends FormRequest
+class UploadRequest extends AuthenticatedRequest
 {
     const array ALLOWED_CONTAINERS = [
         ContainerAliasEnum::LL_POST->value,
         ContainerAliasEnum::TM_TASK->value,
     ];
-
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
 
     /**
      * Get the validation rules that apply to the request.
